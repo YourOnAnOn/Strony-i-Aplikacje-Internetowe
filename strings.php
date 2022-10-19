f5e7286a6654b430b89432619806483768764afd
YourOnAnOn <114405053+YourOnAnOn@users.noreply.github.com>
2022-10-19 11:54:32 +0200

    Add files via upload

--- strings.php
+++ strings.php
@@ -8,15 +8,17 @@
 </head>
 <body>
     <?php 
-        $lenght = 37;
-        $pad_string = "-_-";
-        $findme = "to";
-        define ("DOBRA_RADA" , "Bez chleba, to sie nie najesz.");
-        echo DOBRA_RADA."<br>";
-        echo strlen(DOBRA_RADA)."<br>";
-        echo str_word_count (DOBRA_RADA)."<br>"; 
-        echo strrev (DOBRA_RADA)."<br>";
-        echo strpos (DOBRA_RADA, $findme)."<br>";
+        $lenght = 37; /* tutaj zdefiniowałem zmenną intiger o wartości 37 */
+        $pad_string = "-_-"; #Zmienna varchar o wartości "-_-"
+        $findme = "to"; 
+        /* Tutaj zdefinowałem wartość "to", jest to wartość typu varchar.
+        Tę wartość użyję przy użyciu funkcji strpos która wskazuje pozycje wybranego tekstu w tekście */
+        define ("DOBRA_RADA" , "Bez chleba, to sie nie najesz."); #nowa zmienna - definiuje zmienną DOBRA_RADA
+        echo DOBRA_RADA."<br>"; #po każdej linijce kodu daję <br> żeby ładnie wyglądało na stronie
+        echo strlen(DOBRA_RADA)."<br>"; #W tej linijce wyświetla się długość zdania
+        echo str_word_count (DOBRA_RADA)."<br>"; # w tej linijce liczba słów
+        echo strrev (DOBRA_RADA)."<br>"; #w tej linijce tekst jest napisany od tyłu
+        echo strpos (DOBRA_RADA, $findme)."<br>"; 
         echo str_replace (DOBRA_RADA, "chleba", "mięsa")."<br>";
         echo strtolower (DOBRA_RADA)."<br>";
         echo strtoupper (DOBRA_RADA)."<br>";
